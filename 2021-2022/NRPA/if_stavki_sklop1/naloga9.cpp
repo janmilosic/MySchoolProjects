01b5ae2f99f6d2da29ccdb9445616362801bc228
Jan Milosic <61479658+janmilosic@users.noreply.github.com>
2021-12-13 17:34:10 +0100

    update 13.12

---
+++ 2021-2022/NRPA/if_stavki_sklop1/naloga9.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <math.h>
+using namespace std;
+
+int main()
+{
+
+    int starost;
+
+    cout<<"Vpisite vaso starost: ";
+    cin>>starost;
+
+    if (starost>=18){
+        cout<<"polnoleten si";
+    } else{
+        cout<<"nisi polnoleten";
+    }
+
+    return 0;
+}
