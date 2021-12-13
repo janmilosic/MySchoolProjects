8ad696a67a3f252f42125631c2a48bd827c287cc
Jan Milosic <61479658+janmilosic@users.noreply.github.com>
2021-12-13 17:42:26 +0100

    update 13.12

---
+++ 2021-2022/NRPA/if_stavki_sklop1/naloga10.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <math.h>
+#include <cstdlib>
+#include <ctime>
+
+using namespace std;
+
+int main()
+{
+
+    int neko_stevilo, vnos_stevilo;
+
+    srand(time(NULL));
+    neko_stevilo=rand()%6+1;
+
+    cout<<"Poskusaj uganiti stevilo med 1 in 5: ";
+    cin>>vnos_stevilo;
+
+    if (vnos_stevilo==neko_stevilo){
+        cout<<"BINGOOOOO!";
+    } else{
+        cout<<"vec srece prihodnjic";
+    }
+
+    return 0;
+}
