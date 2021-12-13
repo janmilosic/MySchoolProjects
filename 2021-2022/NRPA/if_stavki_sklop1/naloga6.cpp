a393b0d98a84c598ca39d0f4a34d6784b2d35976
Jan Milosic <61479658+janmilosic@users.noreply.github.com>
2021-12-13 17:12:22 +0100

    update 13.12

---
+++ 2021-2022/NRPA/if_stavki_sklop1/naloga6.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <math.h>
+using namespace std;
+
+int main()
+{
+
+    int st;
+
+    cout<<"Podaj celo stevilo: ";
+    cin>>st;
+
+    if (st==100){
+        cout<<"x je 100";
+    }else {
+        cout<<"stevilo razlicno od 100";
+    }
+
+    return 0;
+}
