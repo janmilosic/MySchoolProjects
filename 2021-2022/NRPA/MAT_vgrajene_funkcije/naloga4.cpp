825c2d19b8dad28d6814a89af2cc494740c06397
Jan Milosic <61479658+janmilosic@users.noreply.github.com>
2021-12-18 19:42:29 +0100

    update 18.12

---
+++ 2021-2022/NRPA/MAT_vgrajene_funkcije/naloga4.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+
+    int n;
+    int nekaj = 0;
+
+
+    cout << "Vnesi neko vrednost n-ja (podas lahko samo liha stevila): " <<endl;
+    cin >> n;
+    if (n % 2 == 0){
+        cout << "Vnesel si sodo stevilo!" << endl;
+    }else {
+        for (int i = 0; i < n; ++i) { // i gre navpicno
+            nekaj = nekaj + 1;
+            for (int j = 0; j < n; ++j) { // j gre vodoravno
+                if (i == n/2 || j == n/2 || i == n/2 || n/2 == j){
+                    cout << nekaj <<"  ";
+                } else {
+                    cout << "0  ";
+                }
+            }
+            cout << "" << endl;
+        }
+    }
+
+    return 0;
+}
