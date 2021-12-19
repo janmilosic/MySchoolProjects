92b77976c7ae8d9cc78ecafedded7dbf23318493
Jan Milosic <61479658+janmilosic@users.noreply.github.com>
2021-12-19 14:19:07 +0100

    update 19.12

---
+++ 2021-2022/NRPA/Naloge_MAT_Nizi/naloga4.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <string>
+#include <algorithm>
+
+using namespace std;
+
+string besedilo;
+string koncni_niz;
+
+string vrniSograsnike(){
+
+    for (int i = 0; i < besedilo.length(); ++i) {
+        if (besedilo[i] != 'A' && besedilo[i] != 'E' && besedilo[i] != 'I' && besedilo[i] != 'O' && besedilo[i] != 'U'){
+        koncni_niz = koncni_niz + besedilo[i];
+        }
+    }
+    return koncni_niz;
+}
+
+int main() {
+
+    cout << "Vnesi poljubno besedilo: " << endl;
+    cin >> besedilo;
+    transform(besedilo.begin(), besedilo.end(),besedilo.begin(), ::toupper);
+
+    koncni_niz = vrniSograsnike();
+    cout << koncni_niz << endl;
+
+    return 0;
+}
