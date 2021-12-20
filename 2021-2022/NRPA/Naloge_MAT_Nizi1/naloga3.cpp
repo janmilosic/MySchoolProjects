4e3b84cffbe5607b57a1fda781b0dfdf893ba650
Jan Milosic <61479658+janmilosic@users.noreply.github.com>
2021-12-20 23:07:24 +0100

    update 20.12

---
+++ 2021-2022/NRPA/Naloge_MAT_Nizi1/naloga3.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <string>
+#include <algorithm>
+
+using namespace std;
+
+string besedilo;
+string cenzura;
+
+void cenzuraSamoglasnikov(){
+
+    for (int i = 0 ; i <= besedilo.length(); i++) {
+        if (besedilo[i] == 'A' || besedilo[i] == 'E' || besedilo[i] == 'I' || besedilo[i] == 'O' || besedilo[i] == 'U'){
+            besedilo[i] = '#';
+        }
+        cenzura = cenzura + besedilo[i];
+    }
+    cout << cenzura <<endl;
+}
+
+int main() {
+
+    cout << "Vnesi poljubno besedilo1: " << endl;
+    getline (cin, besedilo);
+    transform(besedilo.begin(), besedilo.end(),besedilo.begin(), ::toupper);
+
+    cenzuraSamoglasnikov();
+
+    return 0;
+}
