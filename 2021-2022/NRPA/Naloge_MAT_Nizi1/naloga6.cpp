186227a0befa13122524e6eef4fe721535b03a48
Jan Milosic <61479658+janmilosic@users.noreply.github.com>
2021-12-24 17:40:33 +0100

    update 24.12

--- 2021-2022/NRPA/Naloge_MAT_Nizi1/naloga6.cpp
+++ 2021-2022/NRPA/Naloge_MAT_Nizi1/naloga6.cpp
@@ -1,16 +1,16 @@
-//ne dela !!! reci aneje ker on ve
-
 #include <iostream>
 
 using namespace std;
 
-int stevilo;
+long stevilo;
 int array[13];
-int a, b = 7, c = 0, d = 13;
 
-int preveriDavcnoST(){
 
+void preveriemso(){
+int c=0,a;
+int b=7,d=13;
     for (int i = 0; i < 12; i++) {
+
         if (b > 1){
             a = array[i] * b;
         } else {
@@ -21,27 +21,26 @@ int preveriDavcnoST(){
         d--;
         c = c + a;
     }
-
-    c = c + array[12];
+    c=c+array[12];
     if (c % 11 != 0){
         cout << "EMSO je napacna!" << endl;
     } else {
         cout << "EMSO je pravilna!" << endl;
     }
-
 }
 
 int main() {
 
-    cout << "Vnesi poljubno besedilo: " << endl;
+    cout << "Vnesi emso: " << endl;
     cin >> stevilo;
 
-    for (int i = 12; i >= 0; i--) {
-        array[i] = stevilo % 10;
-        stevilo /= 10;
-    }
 
-    preveriDavcnoST();
+for (int i = 12; i >= 0; i--) {
+    array[i] = stevilo % 10;
+    stevilo /= 10;
+}
+
+    preveriemso();
 
     return 0;
 }
