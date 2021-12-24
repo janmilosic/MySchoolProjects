6d380b25565337bec8e83b000540fc454b446015
Jan Milosic <61479658+janmilosic@users.noreply.github.com>
2021-12-24 17:38:57 +0100

    update 24.12

---
+++ 2021-2022/NRPA/Naloge_MAT_Nizi_in_Polja/naloga16.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+#include <time.h>
+
+using namespace std;
+
+void izpisiHarshadovaStevila(){
+
+    srand(time(NULL));
+
+    for (int i = 0; i < 200; ++i) {
+        int stevilo;
+        int a;
+        int vsota = 0;
+
+        for (int i = 0; i < 1; ++i) {
+            stevilo = rand()%900+100;
+        }
+
+        for (int j=1; j < stevilo; j++)
+        {
+            a = stevilo % j;
+            if (a == 0)
+                vsota += j;
+        }
+
+        if (vsota > stevilo){
+            cout<< stevilo <<" is an Abundant number."<<endl;
+        }
+    }
+
+}
+
+int main() {
+
+    izpisiHarshadovaStevila();
+
+    return 0;
+}
